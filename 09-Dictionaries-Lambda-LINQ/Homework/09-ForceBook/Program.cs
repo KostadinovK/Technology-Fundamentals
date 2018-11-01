a6f29671e5c8e5e971116382b33a3b71d9e8973a
KostadinovK <kostadinovk2001@gmail.com>
2018-11-01 19:08:01 +0200

    Added another solution for task 09 for lection 09

--- 09-Dictionaries-Lambda-LINQ/Homework/09-ForceBook/Program.cs
+++ 09-Dictionaries-Lambda-LINQ/Homework/09-ForceBook/Program.cs
@@ -8,90 +8,48 @@ public class Program
 {
 	public static void Main()
 	{
-		Dictionary<string,List<string>> forceBook = new Dictionary<string, List<string>>();
+		Dictionary<string,string> nameToSide = new Dictionary<string, string>();
 
 		string line = Console.ReadLine();
+
 		while (line != "Lumpawaroo")
 		{
-			for (int i = 0; i < line.Length; i++)
+			if (line.Contains(" | "))
 			{
-				if (line[i] == '|')
-				{
-					List<string> args = line.Split(new string[] { " | " }, StringSplitOptions.None).ToList();
-					string forceSide = args[0];
-					string forceUser = args[1];
+				List<string> tokens = line.Split(new string[]{" | "}, StringSplitOptions.None).ToList();
+				string side = tokens[0];
+				string username = tokens[1];
 
-					if (!forceBook.ContainsKey(forceSide))
-					{
-						forceBook[forceSide] = new List<string>();
-						forceBook[forceSide].Add(forceUser);
-					}
-					else
-					{
-						if (!forceBook[forceSide].Contains(forceUser))
-						{
-							forceBook[forceSide].Add(forceUser);
-						}
-					}
-					break;
+				if (!nameToSide.ContainsKey(username))
+				{
+					nameToSide[username] = side;
 				}
 
-				if (line[i] == '>')
-				{
-					List<string> args = line.Split(new string[] { " -> " }, StringSplitOptions.None).ToList();
-					string forceSide = args[1];
-					string forceUser = args[0];
+				
+			}else if (line.Contains(" -> "))
+			{
+				List<string> tokens = line.Split(new string[] { " -> " }, StringSplitOptions.None).ToList();
+				string side = tokens[1];
+				string username = tokens[0];
 
-					if (ForceUserExist(forceUser,forceBook))
-					{
-						RemoveForceUser(forceUser,ref forceBook);
-					}
-					
-					forceBook[forceSide].Add(forceUser);
-					Console.WriteLine($"{forceUser} joins the {forceSide} side!");
-					break;
-				}
+				nameToSide[username] = side;
+
+				Console.WriteLine($"{username} joins the {side} side!");
 			}
+
 			line = Console.ReadLine();
 		}
 
-		foreach (var kvp in forceBook.Where(x => x.Value.Count > 0).OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+		var groupedDict = nameToSide.GroupBy(x => x.Value)
+			.ToDictionary(x => x.Key, x => x.ToDictionary(y => y.Key, y => y.Value));
+
+		foreach (var kvp in groupedDict.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
 		{
 			Console.WriteLine($"Side: {kvp.Key}, Members: {kvp.Value.Count}");
-			kvp.Value.OrderBy(x => x).ToList().ForEach(x => Console.WriteLine("! " + x));
-		}
-
-
-	}
-
-	private static void RemoveForceUser(string forceUser, ref Dictionary<string, List<string>> forceBook)
-	{
-		foreach (var kvp in forceBook)
-		{
-			for (int i = 0; i < kvp.Value.Count; i++)
+			foreach (var nameSidePair in kvp.Value.OrderBy(x => x.Key))
 			{
-				if (forceUser == kvp.Value[i])
-				{
-					kvp.Value.Remove(forceUser);
-					break;
-				}
+				Console.WriteLine($"! {nameSidePair.Key}");
 			}
 		}
-	}
-
-	private static bool ForceUserExist(string forceUser, Dictionary<string, List<string>> forceBook)
-	{
-		foreach (var kvp in forceBook)
-		{
-			for (int i = 0; i < kvp.Value.Count; i++)
-			{
-				if (forceUser == kvp.Value[i])
-				{
-					return true;
-				}
-			}
-		}
-
-		return false;
 	}
 }
